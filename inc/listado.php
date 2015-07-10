4be4a0bfd89e3a583e850fab035566d2188f55e2
Ismael Fernández <isma907@gmail.com>
2015-07-09 21:13:02 -0300

    delete

--- inc/listado.php
+++ inc/listado.php
@@ -1,36 +1,34 @@
 <?php 
 require("conexion.php");
 if (isset($_POST['accion'])){
-$accion = $_POST["accion"];
+	$accion = $_POST["accion"];
 
 
 
-	 if ($accion == "generarListado"){
+	if ($accion == "generarListado"){
 
 		$listado= mysql_query("SELECT * FROM `usuarios`ORDER BY `id` DESC");
 		$contarList= mysql_num_rows($listado);
 		
 		if ($contarList== 0){ echo 'No hay usuarios.';
-		}
-		else{
-			while ( $row=mysql_fetch_array($listado) ) { ?>
-			<tr class="usr<?php echo $row['id'];  ?> ">
+	}
+	else{
+		while ( $row=mysql_fetch_array($listado) ) { ?>
+		<tr class="usr<?php echo $row['id'];  ?> ">
 			<td><?php echo $row['nombre']; ?></td>
 			<td><?php echo $row['apellido']; ?></td>
 			<td><?php echo $row['email']; ?></td>
 			<td>
-				<button class="btn btn-success btn-xs"><i class="fa fa-check"></i></button>
-				<button class="btn btn-primary btn-xs"><i class="fa fa-pencil"></i></button>
 				<button class="btnEliminar remove-item btn btn-danger btn-xs" id="<?php echo $row['id']?>"><i class="fa fa-trash-o "></i></button>
 			</td>
-			</tr>
-			<?php }
-		}
-		
-
-
-
+		</tr>
+		<?php }
 	}
+	
+
+
+
+}
 
 }
 else(die);
